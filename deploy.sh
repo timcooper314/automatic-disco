043ab54f18bd159077fb05d56f8fd815e0af35e9
Tim Cooper <timcooper314@gmail.com>
2021-11-28 11:01:06 +1100

    Created cfn codebuild template, deploy script and codebuild buildspec.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+PROJECT_NAME="tc-test"
+STACK_NAME="${PROJECT_NAME}-hello-world"
+BUCKET_NAME="test-tc-pipeline-artifacts"
+
+echo "AWS SAM packaging..."
+echo ${BUCKET_NAME}
+sam package \
+  --template-file ./template.yaml \
+  --output-template-file packaged.yaml \
+  --s3-bucket ${BUCKET_NAME} \
+  --s3-prefix "${STACK_NAME}"
+echo "Build exit code = $?"
+test "$?" -ne 0 && exit $? || echo "Build completed successfully"
+
+echo "AWS SAM deploying..."
+sam deploy \
+  --template-file packaged.yaml \
+  --stack-name "${STACK_NAME}" \
+  --parameter-overrides Project=${PROJECT_NAME} \
+  --s3-bucket "${BUCKET_NAME}" \
+  --s3-prefix "${STACK_NAME}" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --no-fail-on-empty-changeset \
