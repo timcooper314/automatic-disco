80eff7de6b8ae7b228b0dc975bf71b996c65249f
Tim Cooper <timcooper314@gmail.com>
2022-02-18 08:37:14 +1100

    Run the deploy script directly in gh workflow, by using sam-actions. Remove redundant `sam package` .

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,19 @@ set -e
 
 PROJECT_NAME="tc-test"
 STACK_NAME="${PROJECT_NAME}-hello-world"
-BUCKET_NAME="tc-test-gh-actions-artifacts-291121"
+BUCKET_NAME="tc-test-code-packages"
 
-echo "AWS SAM packaging..."
 echo ${BUCKET_NAME}
-sam package \
-  --template-file ./template.yaml \
-  --output-template-file packaged.yaml \
-  --s3-bucket ${BUCKET_NAME} \
-  --s3-prefix "${STACK_NAME}"
-echo "Build exit code = $?"
-test "$?" -ne 0 && exit $? || echo "Build completed successfully"
 
 echo "AWS SAM deploying..."
 sam deploy \
-  --template-file packaged.yaml \
+  --template-file ./template.yaml \
   --stack-name "${STACK_NAME}" \
   --parameter-overrides Project=${PROJECT_NAME} \
   --s3-bucket "${BUCKET_NAME}" \
   --s3-prefix "${STACK_NAME}" \
   --capabilities CAPABILITY_NAMED_IAM \
-  --no-fail-on-empty-changeset \
+  --no-fail-on-empty-changeset
+
+echo "Deploy exit code = $?"
+test "$?" -ne 0 && exit $? || echo "Deploy completed successfully"
